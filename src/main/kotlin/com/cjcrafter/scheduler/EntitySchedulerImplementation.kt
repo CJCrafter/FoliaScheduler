86150bbab2092dec156147c417802c0b19632be2
Collin Barber <collinjbarber@gmail.com>
2024-08-01 23:55:58 -0400

    add runnable, consumer, and function support to scheduler interfaces

--- src/main/kotlin/com/cjcrafter/scheduler/EntitySchedulerImplementation.kt
+++ src/main/kotlin/com/cjcrafter/scheduler/EntitySchedulerImplementation.kt
@@ -1,6 +1,7 @@
 package com.cjcrafter.scheduler
 
 import java.util.function.Consumer
+import java.util.function.Function
 
 /**
  * Represents a scheduler that can schedule tasks to be run on an entity.
@@ -32,11 +33,11 @@ interface EntitySchedulerImplementation {
     /**
      * Schedules a task to be run on the next tick.
      *
-     * @param consumer The task to run.
+     * @param function The task to run.
      * @param retired The task to run if the entity is retired.
      * @return The task that was scheduled.
      */
-    fun run(consumer: Consumer<TaskImplementation>, retired: Runnable?): TaskImplementation?
+    fun <T : Any> run(function: Function<TaskImplementation<T>, T>, retired: Runnable?): TaskImplementation<T>?
 
     /**
      * Schedules a task to be run on the next tick.
@@ -44,11 +45,73 @@ interface EntitySchedulerImplementation {
      * @param consumer The task to run.
      * @return The task that was scheduled.
      */
-    fun run(consumer: Consumer<TaskImplementation>): TaskImplementation? {
+    fun run(consumer: Consumer<TaskImplementation<Void>>, retired: Runnable?): TaskImplementation<Void>? {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            consumer.accept(task)
+            null
+        }
+        return run(wrapperFunction, retired)
+    }
+
+    /**
+     * Schedules a task to be run on the next tick.
+     *
+     * @param runnable The task to run.
+     * @return The task that was scheduled.
+     */
+    fun run(runnable: Runnable, retired: Runnable?): TaskImplementation<Void>? {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            runnable.run()
+            null
+        }
+        return run(wrapperFunction, retired)
+    }
+
+    /**
+     * Schedules a task to be run on the next tick.
+     *
+     * @param function The task to run.
+     * @return The task that was scheduled.
+     */
+    fun <T : Any> run(function: Function<TaskImplementation<T>, T>): TaskImplementation<T>? {
+        return run(function, null)
+    }
+
+    /**
+     * Schedules a task to be run on the next tick.
+     *
+     * @param consumer The task to run.
+     * @return The task that was scheduled.
+     */
+    fun run(consumer: Consumer<TaskImplementation<Void>>): TaskImplementation<Void>? {
         return run(consumer, null)
     }
 
     /**
+     * Schedules a task to be run on the next tick.
+     *
+     * @param runnable The task to run.
+     * @return The task that was scheduled.
+     */
+    fun run(runnable: Runnable): TaskImplementation<Void>? {
+        return run(runnable, null)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param function The task to run.
+     * @param retired The task to run if the entity is retired.
+     * @param delay The delay before the task is run.
+     * @return The task that was scheduled.
+     */
+    fun <T : Any> runDelayed(
+        function: Function<TaskImplementation<T>, T>,
+        retired: Runnable?,
+        delay: Long,
+    ): TaskImplementation<T>?
+
+    /**
      * Schedules a task to be run after a delay.
      *
      * @param consumer The task to run.
@@ -56,7 +119,51 @@ interface EntitySchedulerImplementation {
      * @param delay The delay before the task is run.
      * @return The task that was scheduled.
      */
-    fun runDelayed(consumer: Consumer<TaskImplementation>, retired: Runnable?, delay: Long): TaskImplementation?
+    fun runDelayed(
+        consumer: Consumer<TaskImplementation<Void>>,
+        retired: Runnable?,
+        delay: Long,
+    ): TaskImplementation<Void>? {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            consumer.accept(task)
+            null
+        }
+        return runDelayed(wrapperFunction, retired, delay)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param runnable The task to run.
+     * @param retired The task to run if the entity is retired.
+     * @param delay The delay before the task is run.
+     * @return The task that was scheduled.
+     */
+    fun runDelayed(
+        runnable: Runnable,
+        retired: Runnable?,
+        delay: Long,
+    ): TaskImplementation<Void>? {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            runnable.run()
+            null
+        }
+        return runDelayed(wrapperFunction, retired, delay)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param function The task to run.
+     * @param delay The delay before the task is run.
+     * @return The task that was scheduled.
+     */
+    fun <T : Any> runDelayed(
+        function: Function<TaskImplementation<T>, T>,
+        delay: Long,
+    ): TaskImplementation<T>? {
+        return runDelayed(function, null, delay)
+    }
 
     /**
      * Schedules a task to be run after a delay.
@@ -65,12 +172,45 @@ interface EntitySchedulerImplementation {
      * @param delay The delay before the task is run.
      * @return The task that was scheduled.
      */
-    fun runDelayed(consumer: Consumer<TaskImplementation>, delay: Long): TaskImplementation? {
+    fun runDelayed(
+        consumer: Consumer<TaskImplementation<Void>>,
+        delay: Long,
+    ): TaskImplementation<Void>? {
         return runDelayed(consumer, null, delay)
     }
 
     /**
-     * Schedules a task to be run after a delay and then repeatedly after a period.
+     * Schedules a task to be run after a delay.
+     *
+     * @param runnable The task to run.
+     * @param delay The delay before the task is run.
+     * @return The task that was scheduled.
+     */
+    fun runDelayed(
+        runnable: Runnable,
+        delay: Long,
+    ): TaskImplementation<Void>? {
+        return runDelayed(runnable, null, delay)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param function The task to run.
+     * @param retired The task to run if the entity is retired.
+     * @param delay The delay before the task is run.
+     * @param period The period between each run.
+     * @return The task that was scheduled.
+     */
+    fun <T : Any> runAtFixedRate(
+        function: Function<TaskImplementation<T>, T>,
+        retired: Runnable?,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<T>?
+
+    /**
+     * Schedules a task to be run after a delay.
      *
      * @param consumer The task to run.
      * @param retired The task to run if the entity is retired.
@@ -78,18 +218,87 @@ interface EntitySchedulerImplementation {
      * @param period The period between each run.
      * @return The task that was scheduled.
      */
-    fun runAtFixedRate(consumer: Consumer<TaskImplementation>, retired: Runnable?, delay: Long, period: Long): TaskImplementation?
+    fun runAtFixedRate(
+        consumer: Consumer<TaskImplementation<Void>>,
+        retired: Runnable?,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<Void>? {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            consumer.accept(task)
+            null
+        }
+        return runAtFixedRate(wrapperFunction, retired, delay, period)
+    }
 
     /**
-     * Schedules a task to be run after a delay and then repeatedly after a period.
+     * Schedules a task to be run after a delay.
+     *
+     * @param runnable The task to run.
+     * @param retired The task to run if the entity is retired.
+     * @param delay The delay before the task is run.
+     * @param period The period between each run.
+     * @return The task that was scheduled.
+     */
+    fun runAtFixedRate(
+        runnable: Runnable,
+        retired: Runnable?,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<Void>? {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            runnable.run()
+            null
+        }
+        return runAtFixedRate(wrapperFunction, retired, delay, period)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param function The task to run.
+     * @param delay The delay before the task is run.
+     * @param period The period between each run.
+     * @return The task that was scheduled.
+     */
+    fun <T : Any> runAtFixedRate(
+        function: Function<TaskImplementation<T>, T>,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<T>? {
+        return runAtFixedRate(function, null, delay, period)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
      *
      * @param consumer The task to run.
      * @param delay The delay before the task is run.
      * @param period The period between each run.
      * @return The task that was scheduled.
      */
-    fun runAtFixedRate(consumer: Consumer<TaskImplementation>, delay: Long, period: Long): TaskImplementation? {
+    fun runAtFixedRate(
+        consumer: Consumer<TaskImplementation<Void>>,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<Void>? {
         return runAtFixedRate(consumer, null, delay, period)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param runnable The task to run.
+     * @param delay The delay before the task is run.
+     * @param period The period between each run.
+     * @return The task that was scheduled.
+     */
+    fun runAtFixedRate(
+        runnable: Runnable,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<Void>? {
+        return runAtFixedRate(runnable, null, delay, period)
     }
 }
 
--- src/main/kotlin/com/cjcrafter/scheduler/SchedulerImplementation.kt
+++ src/main/kotlin/com/cjcrafter/scheduler/SchedulerImplementation.kt
@@ -1,6 +1,7 @@
 package com.cjcrafter.scheduler
 
 import java.util.function.Consumer
+import java.util.function.Function
 
 /**
  * Represents a scheduler that can schedule tasks to be run.
@@ -14,12 +15,52 @@ interface SchedulerImplementation {
     fun execute(run: Runnable)
 
     /**
-     * Schedules a task to be run on the next tick.
+     * Schedules a task to be run after a delay.
+     *
+     * @param function The task to run.
+     * @return The task that was scheduled.
+     */
+    fun <T : Any> run(function: Function<TaskImplementation<T>, T>): TaskImplementation<T>
+
+    /**
+     * Schedules a task to be run after a delay.
      *
      * @param consumer The task to run.
      * @return The task that was scheduled.
      */
-    fun run(consumer: Consumer<TaskImplementation>): TaskImplementation
+    fun run(consumer: Consumer<TaskImplementation<Void>>): TaskImplementation<Void> {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            consumer.accept(task)
+            null
+        }
+        return run(wrapperFunction)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param runnable The task to run.
+     * @return The task that was scheduled.
+     */
+    fun run(runnable: Runnable): TaskImplementation<Void> {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            runnable.run()
+            null
+        }
+        return run(wrapperFunction)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param function The task to run.
+     * @param delay The delay in ticks before the task is run.
+     * @return The task that was scheduled.
+     */
+    fun <T : Any> runDelayed(
+        function: Function<TaskImplementation<T>, T>,
+        delay: Long,
+    ): TaskImplementation<T>
 
     /**
      * Schedules a task to be run after a delay.
@@ -28,15 +69,84 @@ interface SchedulerImplementation {
      * @param delay The delay in ticks before the task is run.
      * @return The task that was scheduled.
      */
-    fun runDelayed(consumer: Consumer<TaskImplementation>, delay: Long): TaskImplementation
+    fun runDelayed(
+        consumer: Consumer<TaskImplementation<Void>>,
+        delay: Long,
+    ): TaskImplementation<Void> {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            consumer.accept(task)
+            null
+        }
+        return runDelayed(wrapperFunction, delay)
+
+    }
 
     /**
-     * Schedules a task to be run after a delay and then repeatedly after a period.
+     * Schedules a task to be run after a delay.
      *
-     * @param consumer The task to run.
+     * @param runnable The task to run.
      * @param delay The delay in ticks before the task is run.
-     * @param period The period in ticks between each run.
      * @return The task that was scheduled.
      */
-    fun runAtFixedRate(consumer: Consumer<TaskImplementation>, delay: Long, period: Long): TaskImplementation
+    fun runDelayed(
+        runnable: Runnable,
+        delay: Long,
+    ): TaskImplementation<Void> {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            runnable.run()
+            null
+        }
+        return runDelayed(wrapperFunction, delay)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param function The task to run.
+     * @param delay The delay before the task is run.
+     * @param period The time unit of the delay.
+     */
+    fun <T : Any> runAtFixedRate(
+        function: Function<TaskImplementation<T>, T>,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<T>
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param consumer The task to run.
+     * @param delay The delay before the task is run.
+     * @param period The time unit of the delay.
+     */
+    fun runAtFixedRate(
+        consumer: Consumer<TaskImplementation<Void>>,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<Void> {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            consumer.accept(task)
+            null
+        }
+        return runAtFixedRate(wrapperFunction, delay, period)
+    }
+
+    /**
+     * Schedules a task to be run after a delay.
+     *
+     * @param runnable The task to run.
+     * @param delay The delay before the task is run.
+     * @param period The time unit of the delay.
+     */
+    fun runAtFixedRate(
+        runnable: Runnable,
+        delay: Long,
+        period: Long,
+    ): TaskImplementation<Void> {
+        val wrapperFunction = Function<TaskImplementation<Void>, Void> { task ->
+            runnable.run()
+            null
+        }
+        return runAtFixedRate(wrapperFunction, delay, period)
+    }
 }
